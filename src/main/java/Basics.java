13c9e353787ccd952f7cdaea715ce6a27d6d5377
alini <alinikan188@gmail.com>
2023-06-04 21:05:44 -0400

    This is my completed version of basic.java Ali Agharezakashi

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -8,7 +8,7 @@
  */
 
 /**
- *  1. In Java, no code can exist outside of a class. Unlike Python which uses
+ *  1. In Java, no code can exist outside a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
  *
  *    For this exercise, all of our code is within the Basics class.
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,10 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i=10; i>=0;i--)
+        {
+            System.out.println("Current count: "+i);
+        }
 
 
     }
@@ -143,6 +149,11 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+       String[] my_array = to_split.split(" ");
+        for (int i=0; i< my_array.length; i++)
+        {
+            ret.append(my_array[i].charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +181,16 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        if (arr.length<=1)
+        {
+            return 0;
+        }
+        else {
+            for (int i=1;i<arr.length;i+=2)
+            {
+                current_sum += arr[i];
+            }
+        }
 
         return current_sum;
     }
